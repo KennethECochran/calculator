7f70516753f4bc41c1a5df7853cf660355246dbd
KennethC <91973026+KennethECochran@users.noreply.github.com>
2023-09-14 13:15:02 -0400

    Created operate function that takes in 2 numbers and an operand and performs the relevant calculation.

--- script.js
+++ script.js
@@ -15,5 +15,28 @@ function multiply(operand1, operand2){
 }
 
 function divide(operand1, operand2){
-    return(operand1 / operand2);
+    if(operand1 == 0 || operand2 == 0){
+        return("Error: You cannot divide by 0 :(");
+    }
+    else{
+        return(operand1 / operand2);
+    }
+}
+
+function operate(operand1, operation, operand2){
+    if(operation === '+'){
+        return add(operand1, operand2);
+    }
+    else if(operation === '-'){
+        return subtract(operand1, operand2);
+    }
+    else if(operation === '*'){
+        return multiply(operand1, operand2);
+    }
+    else if(operation === '/'){
+        return divide(operand1, operand2);
+    }
+    else{
+        return("Error: You didn't enter a valid operation");
+    }
 }
