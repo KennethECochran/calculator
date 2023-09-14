103c8dc3f0a6c2eca4f8f64038f7112880d35556
KennethC <91973026+KennethECochran@users.noreply.github.com>
2023-09-14 18:21:25 -0400

    Added function to update display, function to clear display, added listeners to do basic math operations

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let operand1;
-let operation;
-let operand2;
 
 function add(operand1, operand2){
     return(operand1 + operand2);
@@ -16,7 +13,8 @@ function multiply(operand1, operand2){
 
 function divide(operand1, operand2){
     if(operand1 == 0 || operand2 == 0){
-        return("Error: You cannot divide by 0 :(");
+        alert("You can't divide by 0 :(");
+        return(0);
     }
     else{
         return(operand1 / operand2);
@@ -37,6 +35,41 @@ function operate(operand1, operation, operand2){
         return divide(operand1, operand2);
     }
     else{
-        return("Error: You didn't enter a valid operation");
+        return("Error");
     }
 }
+
+function updateDisplay(value){
+    displayValue.textContent+= value;
+}
+
+function clear(){
+    displayValue.textContent = '';
+}
+
+const displayValue = document.querySelector('#result');
+
+const buttons = document.querySelectorAll('.key');
+buttons.forEach((button) => {
+    button.addEventListener('click', () =>{
+        updateDisplay(button.id);
+    })
+})
+
+const clear_button = document.querySelector('#clear');
+clear_button.addEventListener('click', () =>{
+    clear();
+})
+
+const equals_button = document.querySelector('#equal');
+equals_button.addEventListener('click',() => {
+    let input_array = displayValue.textContent.split(/([+*\-/])/);
+    console.log(input_array);
+    let operand1 = parseInt(input_array[0]);
+    let operation = input_array[1];
+    let operand2 = parseInt(input_array[2]);
+    clear();
+    updateDisplay(operate(operand1,operation,operand2));
+})
+
+
